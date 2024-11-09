19c6567dfeb7b8ea23e612f9dad7891b8f0ac43f
Scott Larsen <sslarsen411@gmail.com>
2024-11-09 05:15:58 -0800

    Update for local and production redirect URLs

--- app/Http/Controllers/ShortyController.php
+++ app/Http/Controllers/ShortyController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\LocationLink;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class ShortyController extends Controller{
     public function show($shortUrl){
+        $redirect = 'https://twoshakes.app';
+        if (App::environment('local')) {
+           $redirect = 'https://tsrapp.test';
+        }
         $link = LocationLink::where('link', $shortUrl)->first();
         if (!$link)
             abort(404);        
-        return redirect("https://tsrapp.test/?loc={$link->location_id}");
+        return redirect("$redirect/?loc={$link->location_id}");
     }
 }
